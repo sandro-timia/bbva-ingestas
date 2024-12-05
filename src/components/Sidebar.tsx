53517f1ee6e4f1516903fb350ca613e2f98a8065
Sandro Calzada <Sandro.calzada@timia.ai>
2024-12-05 13:41:36 -0500

    mis tablas

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,19 +1,32 @@
 import { 
-  HomeIcon, 
+  FunnelIcon, 
   UserIcon, 
   GlobeAltIcon 
 } from "@heroicons/react/24/outline";
 import Link from "next/link";
+import { usePathname } from 'next/navigation';
 
 export default function Sidebar() {
+  const pathname = usePathname();
+
   return (
     <div className="fixed left-0 top-0 h-screen w-[60px] bg-[#0A3977] flex flex-col justify-between py-6">
       {/* Top Section */}
       <div className="flex flex-col items-center space-y-4">
-        <Link href="/" className="p-3 hover:bg-[#0067B2]/50 rounded-lg transition-colors">
-          <HomeIcon className="w-6 h-6 text-white" />
+        <Link 
+          href="/" 
+          className={`p-3 rounded-lg transition-colors ${
+            pathname === '/' ? 'bg-[#0067B2]' : 'hover:bg-[#0067B2]/50'
+          }`}
+        >
+          <FunnelIcon className="w-6 h-6 text-white" />
         </Link>
-        <Link href="/tablas" className="p-3 hover:bg-[#0067B2]/50 rounded-lg transition-colors">
+        <Link 
+          href="/tablas" 
+          className={`p-3 rounded-lg transition-colors ${
+            pathname === '/tablas' ? 'bg-[#0067B2]' : 'hover:bg-[#0067B2]/50'
+          }`}
+        >
           <UserIcon className="w-6 h-6 text-white" />
         </Link>
       </div>
